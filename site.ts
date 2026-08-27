cffa2263dc727bb440ccc3f0eb61461fc9f5e507
bgkcogito <dakyeong0207@gmail.com>
2026-08-27 09:55:04 +0900

    Add files via upload

--- site.ts
+++ site.ts
@@ -6,6 +6,6 @@ export const site = {
   phone: "010-3015-2717",
   email: "bgkcogito@naver.com",
   businessNumber: "692-75-00296",
-  address: "충청남도 천안시 서북구 천일고2길 113-16, 101동 404호",
+  address: "경기도 화성시 동탄구 동탄중심상가2길 26-21, 603-S206호(반송동, 로드프라자)",
   businessType: "운수 및 창고업 · 택배업 · 도매 및 소매업 · 전자상거래 소매업",
 };
